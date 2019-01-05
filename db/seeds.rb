6cdd7b2c4bdd738a35a8a211cee31dcb4e0f74ac
rendnil <andrew.lindner.15@gmail.com>
2019-01-05 11:50:29 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Post.destroy_all
+
+puts "making users"
+  10.times do
+    User.create()
+  end
+puts "done making users"
+  20.times do
+    Post.create(user_id: User.all.sample.id, title:Faker::GreekPhilosophers.quote, content:Faker::FamousLastWords.last_words, upvotes:rand(100), downvotes:rand(100))
+  end
+
+
+
+puts "making posts"
+puts "done making posts"
