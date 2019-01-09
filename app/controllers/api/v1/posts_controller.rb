b2fe3bd0387f26882525ffaaa6c3c337e5ebb923
rendnil <andrew.lindner.15@gmail.com>
2019-01-09 13:12:24 -0500

    added post update to controller and routes

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,6 +5,19 @@ class Api::V1::PostsController < ApplicationController
     render json: @posts
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    render json: @post
+
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:upvotes, :downvotes)
+  end
+
 
 
 end
