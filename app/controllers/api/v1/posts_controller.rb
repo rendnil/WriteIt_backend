f83fc28d2db53382f994fdc3a3754efdd741f021
rendnil <andrew.lindner.15@gmail.com>
2019-01-14 15:22:48 -0500

    added skeletal create route to post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,6 +5,18 @@ class Api::V1::PostsController < ApplicationController
     render json: @posts
   end
 
+  def create
+    @post = Post.create(user_id: post_params["user_id"],title: post_params["title"],
+    content: post_params["content"], upvotes: 0, downvotes: 0)
+
+    if @post.valid?
+      render json: @post, state: :created
+    else
+      render json: { error: 'Failed to Execute Trade' }, status: :not_acceptable
+    end
+
+  end
+
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
@@ -15,7 +27,7 @@ class Api::V1::PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:upvotes, :downvotes)
+    params.require(:post).permit(:user_id, :title, :content, :upvotes, :downvotes)
   end
 
 
