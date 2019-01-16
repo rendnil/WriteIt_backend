3ff5864a87be70141c2d4481f20deeb46b677413
rendnil <andrew.lindner.15@gmail.com>
2019-01-16 18:09:18 -0500

    implemented auth controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,22 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
   def index
     @users = User.all
     render json: @users
   end
 
+  def profile
+    # user is logged in and needs profile
+    #current_user is from ApplicationController
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: @user, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: {user: UserSerializer.new(@user), jwt:@token}, status: :created
     else
       render json: {error: "failed to create"}, status: :not_acceptable
     end
