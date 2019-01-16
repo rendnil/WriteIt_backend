1b92de330ec95fdc2663d501e07e08432a43495d
rendnil <andrew.lindner.15@gmail.com>
2019-01-16 16:40:20 -0500

    updated user db and add users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_05_163705) do
+ActiveRecord::Schema.define(version: 2019_01_16_213608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_01_05_163705) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
