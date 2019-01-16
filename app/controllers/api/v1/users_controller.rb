220df9bca572de399e577cb8a994b7e5a3de34ef
rendnil <andrew.lindner.15@gmail.com>
2019-01-16 16:59:45 -0500

    added user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: @user, status: :created
+    else
+      render json: {error: "failed to create"}, status: :not_acceptable
+    end
+  end
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:password)
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,27 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts, only: [:index, :update, :create]
+      resources :users, only: [:index, :create]
+      post "/login", to: 'auth#create'
+      get '/profile', to: 'users#profile'
     end
   end
 end
+
+
+
+
+# fetch("http://localhost:3000/api/v1/users",{
+#   method: "POST",
+#   headers:{
+#     "Accept": "application/json",
+#     "Content-Type": "application/json"
+#   },
+#   body:JSON.stringify({
+#     user:{
+#       username:"andrew",
+#       password:"hey"
+#     }
+#     })})
+# .then(r=>r.json())
+# .then(r=>console.log(r))
