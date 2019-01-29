be3c35968ac7dac577bbc369889d86e56e0c7ead
rendnil <andrew.lindner.15@gmail.com>
2019-01-28 21:47:09 -0500

    votes controller w/routes

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -2,9 +2,27 @@ class Api::V1::VotesController < ApplicationController
   skip_before_action :authorized, only: [:index, :create, :update]
 
   def index
-    @votes = Post.all
+    @votes = Vote.all
     render json: @votes
   end
+
+  def create
+    @vote = Vote.create(vote_params)
+
+    if @vote.valid?
+      render json: @vote, state: :created
+    else
+      render json: { error: 'Failed to Execute Trade' }, status: :not_acceptable
+    end
+
+  end
+
+
+  private
+  def vote_params
+    params.require(:vote).permit(:post_id, :voter_id, :upvote)
+  end
+
 
 
 
