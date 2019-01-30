af50cdc5bc60fcd11324630cc6aeba9e1f06b38c
rendnil <andrew.lindner.15@gmail.com>
2019-01-29 22:18:55 -0500

    added comments route and controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PostsController < ApplicationController
-  skip_before_action :authorized, only: [:index, :create, :update]
+  skip_before_action :authorized, only: [:index, :create, :update, :comments]
 
   def index
     @posts = Post.all
@@ -14,13 +14,18 @@ class Api::V1::PostsController < ApplicationController
     else
       render json: { error: 'Failed to create post' }, status: :not_acceptable
     end
-
   end
 
   def update
     @post = Post.find(params[:id])
     @post.update(post_params)
     render json: @post
+  end
+
+  def comments
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    render json: @comments
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :create]
       post "/login", to: 'auth#create'
       get '/profile', to: 'users#profile'
+      get '/posts/:id/comments', to: 'posts#comments'
     end
   end
 end
